f16e6123ddb6a48f16ec07285b77e1dfafa69b34
Source Academy User <No public email provided>
2022-01-18 09:24:32 -0800

    Changes made from Source Academy

--- Lecture1.js
+++ Lecture1.js
@@ -1,9 +1,21 @@
 // Type your program in here!
-// 486 ;
+// 486;
 // 5 * 99;
 // 25 - (4+2) * 3;
 // (2 + 4 * 6) * (3 + 12);
-//math_PI;
+// math_PI;
 // math_sqrt(15);
-const size = 2;
-5 * size;
+// const size = 2;
+// 5 * size;
+/*function square(x) {
+    return x * x;
+}
+square(7);
+*/
+
+//square(square(3));
+true;
+false;
+function adult(x) {
+    return x >= 18;
+}
