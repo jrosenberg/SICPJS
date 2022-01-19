af27ab07d34bc138c39b18471ebd5aea0ad7ecde
Source Academy User <No public email provided>
2022-01-18 16:13:51 -0800

    Changes made from Source Academy

---
+++ Studio-s1.js
@@ -0,0 +1,15 @@
+// Type your program in here!
+function biggie_size(x) {
+    return x + 4;
+}
+biggie_size(1);
+
+function unbiggie_size(x) {
+    return x - 4;
+}
+unbiggie_size(5);
+
+function is_biggie_size(x) {
+    x>=5 ? true : false;
+}
+is_biggie_size(5);
