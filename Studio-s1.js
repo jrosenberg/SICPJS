9e0c0bd19b2d77e8469f2cb0207bc538c553875d
Source Academy User <No public email provided>
2022-01-18 16:25:59 -0800

    Changes made from Source Academy

--- Studio-s1.js
+++ Studio-s1.js
@@ -1,15 +1,36 @@
-// Type your program in here!
-function biggie_size(x) {
-    return x + 4;
+// Qn 1
+
+function biggie_size(meal) {
+    return meal > 0 && meal < 9
+        ? meal + 4
+        : 0;
 }
 biggie_size(1);
 
-function unbiggie_size(x) {
-    return x - 4;
+// Qn 2
+
+function unbiggie_size(meal) {
+    return meal - 4;
 }
 unbiggie_size(5);
 
-function is_biggie_size(x) {
-    x>=5 ? true : false;
+// Qn 3
+
+function is_biggie_size(meal) {
+    return meal>=5 ? true : false;
 }
 is_biggie_size(5);
+is_biggie_size(3);
+
+// Qn 4
+
+function combo_price(meal) {
+    return is_biggie_size(meal)
+        ? 0.50 + (1.17 * unbiggie_size(meal))
+        : 1.17 * meal;
+}
+combo_price(3);
+combo_price(5);
+
+// Qn 5
+
